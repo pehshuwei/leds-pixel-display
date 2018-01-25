045177a0fcdb3ab5978011cfad9dc3eda0f097c5
shuwei97 <shuwei.peh@gmail.com>
2018-01-25 18:14:40 +0800

    weather icons built, get weather information from web done

--- test-weather_web/test-weather_web.ino
+++ test-weather_web/test-weather_web.ino
@@ -1,4 +1,6 @@
+//network & weather
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h> 
 #include <ArduinoJson.h>
 //display
 #include <FastLED.h>
@@ -9,25 +11,28 @@
 #define LED_PIN        3
 #define COLOR_ORDER    RGB
 #define CHIPSET        WS2811
+#define NUM_LEDS 100
 #define MATRIX_WIDTH   10
 #define MATRIX_HEIGHT  10
 #define MATRIX_TYPE    VERTICAL_ZIGZAG_MATRIX
-
+//weather
 #define TEMPBUFSZ 10
 #define WEATBUFSZ 15
-
+//display
+CRGB icon_leds[NUM_LEDS];
 cLEDMatrix<MATRIX_WIDTH, MATRIX_HEIGHT, MATRIX_TYPE> leds;
 cLEDText ScrollingMsg;
-
-const char* ssid     = "iPhone";      // SSID of local network
-const char* password = "shuwei97";   // Password on network
+//network
+ESP8266WiFiMulti wifiMulti;     // Create an instance of the ESP8266WiFiMulti class, called 'wifiMulti'
+//const char* ssid     = "iPhone";      // SSID of local network
+//const char* password = "shuwei97";   // Password on network
 String APIKEY = "c3ddf9e303d30d910149f9ba1f9a6de4";
 String CityID = "1735106"; 
 
 WiFiClient client;
 char servername[]="api.openweathermap.org";  // remote server we will connect to
 String result;
-
+//weather
 int  counter = 30;
 
 String weather = "";
@@ -37,6 +42,7 @@ String weather = "";
 float Temperature;
 //float Humidity;
 //float Pressure;
+int weatherId;
 
 //display
 unsigned char connecting[] = "  connecting  ";
@@ -44,7 +50,30 @@ unsigned char conn[] = "  connected   ";
 unsigned char getting[] = "  getting data  ";
 unsigned char connfail[] = "  connection failed  ";
 unsigned char parsefail[] = "  parseObject() failed  ";
-unsigned char psc[] = { EFFECT_HSV_AH "\x00\xff\xff\xff\xff\xff" " PSC" };
+//unsigned char psc[] = { EFFECT_HSV_AH "\x00\xff\xff\xff\xff\xff" " PSC" };
+
+//weather icon
+//Clear
+int sun[] = {4,5,11,12,14,15,17,18,21,22,27,28,34,35,40,41,43,44,45,46,48,49,50,51,53,54,55,56,58,59,64,65,71,72,77,78,81,82,84,85,87,88,94,95};
+//Cloud
+int cloud[] = {2,3,4,13,14,15,16,17,22,23,24,25,26,33,34,35,36,37,42,43,44,45,54,55,56,57,62,63,64,65,77,76,82,83};
+int smallcloud[] = {46,52,53,66,67,68,71,72,73,74,85,86,87,93,94};
+//Condition cloud
+int condition_cloud[] = {13,14,25,26,27,31,32,33,34,45,46,47,48,52,53,54,65,66,67,73,74,85,86,93,94};
+int condition_smallcloud[] = {51,68,69,70,71,72,87,88,92};
+//Drizzle
+int drizzle[] = {18,24,42,76,80};
+//Rain
+int rain[] = {0,1,3,4,20,22,23,41,42,44,60,61,63,64,82,83,99};
+//Thunder
+int thunder[] = {15,20,21,23,24,25,33,34,36,37,38,42,46,61,62,64,74,75,76,77,83,85};
+//Atmosphere
+int atmosphere[] = {1,4,7,11,14,17,23,26,29,31,34,37,41,44,47,51,54,57,63,66,69,71,74,77,81,84,87,91,94,97};
+//Snow
+int snow_frame[] = {0,1,2,3,4,5,6,7,8,9,10,29,30,49,50,69,70,89,90};
+int snow_flake[] = {13,15,17,21,24,27,31,33,35,37,39,43,44,45,51,52,53,54,56,57,58,59,63,64,65,71,73,75,77,79,81,84,87,93,95,97};
+//Additional & Extreme
+int AE_void[] = {0,1,2,7,8,9,10,11,18,19,20,29,41,42,44,45,46,47,48,51,52,53,54,55,57,58,70,79,80,81,88,89,90,91,92,97,98,99};
 
 void setup() {
   //Serial(57600)
@@ -58,6 +87,9 @@ void setup() {
   ScrollingMsg.Init(&leds, leds.Width(), ScrollingMsg.FontHeight() + 1, 0, 1);
   ScrollingMsg.SetText((unsigned char *)connecting, sizeof(connecting) - 1);
 
+  wifiMulti.addAP("PSC STAFF", "staff.psc321");   // add Wi-Fi networks you want to connect to
+  wifiMulti.addAP("iPhone", "shuwei97");
+
   //display "connecting"
   for(int x=0;x<(sizeof(connecting)*5);x++)
   {
@@ -68,7 +100,17 @@ void setup() {
       delay(100);
   }
   
-  WiFi.begin(ssid, password);
+//  WiFi.begin(ssid, password);
+  int i = 0;
+  while (wifiMulti.run() != WL_CONNECTED) { // Wait for the Wi-Fi to connect: scan for Wi-Fi networks, and connect to the strongest of the networks above
+    delay(250);
+    Serial.print('.');
+  }
+  Serial.println('\n');
+  Serial.print("Connected to ");
+  Serial.println(WiFi.SSID());              // Tell us what network we're connected to
+  Serial.print("IP address:\t");
+  Serial.println(WiFi.localIP());           // Send the IP address of the ESP8266 to the computer
 
   //display "connecting"
   while (WiFi.status() != WL_CONNECTED) {
@@ -107,7 +149,7 @@ void loop() {
     else
     {
         counter++;
-        displayWeather(Temperature,weather);
+        displayWeather(Temperature,weather,weatherId);
         delay(100);
     }
 }
@@ -176,10 +218,12 @@ void getWeatherData() //client function to send/receive GET request data.
   float temperature = root["main"]["temp"];
 //  float humidity = root["main"]["humidity"];
   String mainWeather = root["weather"]["main"];
+  int weather_id = root["weather"]["id"];
 //  String description = root["weather"]["description"];
 //  float pressure = root["main"]["pressure"];
 
     weather = mainWeather;
+    weatherId = weather_id;
 //  weatherDescription = description;
 //  weatherLocation = location;
 //  Country = country;
@@ -189,7 +233,7 @@ void getWeatherData() //client function to send/receive GET request data.
 
 }
 
-void displayWeather(float Temperature, String weather)
+void displayWeather(float Temperature, String weather, int weatherId)
 {    
     //Temperature
     char dest1[TEMPBUFSZ] = "  "; //set space in front for scroll
@@ -201,17 +245,17 @@ void displayWeather(float Temperature, String weather)
     strcpy ((char*)temp, dest1); //convert char array dest1 to unsigned char array temp
 
     //weather
-    Serial.println(weather.length());
-    char dest2[WEATBUFSZ] = "  "; //set space in front for scroll
-    char src2[WEATBUFSZ]; //to store weather
-    weather.toCharArray(src2,WEATBUFSZ); //convert String to char array
-    strcat(dest2,src2); //concat dest2 and src2 "  weather" to dest2
-    unsigned char weat[WEATBUFSZ] = "";  //create unsigned char array
-    strcpy ((char*)weat, dest2); //convert char array dest2 to unsigned char array weat
+//    char dest2[WEATBUFSZ] = "  "; //set space in front for scroll
+//    char src2[WEATBUFSZ]; //to store weather
+//    weather.toCharArray(src2,WEATBUFSZ); //convert String to char array
+//    strcat(dest2,src2); //concat dest2 and src2 "  weather" to dest2
+//    unsigned char weat[WEATBUFSZ] = "";  //create unsigned char array
+//    strcpy ((char*)weat, dest2); //convert char array dest2 to unsigned char array weat
 
     //Serial(57600)
     Serial.println(dest1);
-    Serial.println(dest2);
+    Serial.println(weather);
+    Serial.println(weatherId);
     
     //display temperature
     ScrollingMsg.SetText((unsigned char *)temp, sizeof(temp) - 1);
@@ -225,28 +269,69 @@ void displayWeather(float Temperature, String weather)
         delay(200);
     }
 
-    //display description
-    ScrollingMsg.SetText((unsigned char *)weat, sizeof(weat) - 1);
-    ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0x00, 0x8b, 0x8b);
-    for(int y=0;y<((sizeof(weat))*5);y++)
+    //display weather
+//    ScrollingMsg.SetText((unsigned char *)weat, sizeof(weat) - 1);
+//    ScrollingMsg.SetTextColrOptions(COLR_RGB | COLR_SINGLE, 0x00, 0x8b, 0x8b);
+//    for(int y=0;y<((sizeof(weat))*6);y++)
+//    {
+//        if (ScrollingMsg.UpdateText() == -1)
+//          ScrollingMsg.SetText((unsigned char *)weat, sizeof(weat) - 1);
+//        else
+//          FastLED.show();
+//        delay(200);
+//    }
+
+    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(icon_leds, NUM_LEDS);
+    //display weather icon
+    if(weatherId>=200 && weatherId<300)
     {
-        if (ScrollingMsg.UpdateText() == -1)
-          ScrollingMsg.SetText((unsigned char *)weat, sizeof(weat) - 1);
-        else
-          FastLED.show();
-        delay(200);
+      displayThunder();
     }
+    else if(weatherId>=300 && weatherId<400)
+    {
+      displayDrizzle();
+    }
+    else if(weatherId>=500 && weatherId<600)
+    {
+      displayRain();
+    }
+    else if(weatherId>=600 && weatherId<700)
+    {
+      displaySnow();
+    }
+    else if(weatherId>=700 && weatherId<800)
+    {
+      displayAtmosphere();
+    }
+    else if(weatherId==800)
+    {
+      displayClear();
+    }
+    else if(weatherId>800 && weatherId<900)
+    {
+      displayCloud();
+    }
+    else if(weatherId>=900 && weatherId<910)
+    {
+      displayExtreme();
+    }
+    else if(weatherId>=910 && weatherId<1000)
+    {
+      displayAdditional();
+    }
+    FastLED.clear(true);
+    FastLED.addLeds<CHIPSET, LED_PIN, COLOR_ORDER>(leds[0], leds.Size());
 
     //display psc
-    ScrollingMsg.SetText((unsigned char *)psc, sizeof(psc) - 1);
-    for(int z=0;z<((sizeof(psc))*5);z++)
-    {
-        if (ScrollingMsg.UpdateText() == -1)
-          ScrollingMsg.SetText((unsigned char *)psc, sizeof(psc) - 1);
-        else
-          FastLED.show();
-        delay(200);
-    }
+//    ScrollingMsg.SetText((unsigned char *)psc, sizeof(psc) - 1);
+//    for(int z=0;z<((sizeof(psc))*5);z++)
+//    {
+//        if (ScrollingMsg.UpdateText() == -1)
+//          ScrollingMsg.SetText((unsigned char *)psc, sizeof(psc) - 1);
+//        else
+//          FastLED.show();
+//        delay(200);
+//    }
 }
 
 void displayGettingData()
@@ -265,4 +350,116 @@ void displayGettingData()
   Serial.println("getting data");
 }
 
+void displayClear(){
+  //Clear
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<45; i++){
+    icon_leds[sun[i]] = CRGB::Orange;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
 
+void displayCloud(){
+  //Cloud
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<34; i++){
+    icon_leds[cloud[i]] = CRGB::Cyan;
+  }
+  for(int i=0; i<15; i++){
+    icon_leds[smallcloud[i]] = CRGB::RoyalBlue;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displayDrizzle(){
+  //Drizzle
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<25; i++){
+    icon_leds[condition_cloud[i]] = CRGB::RoyalBlue;
+  }
+  for(int i=0; i<9; i++){
+    icon_leds[condition_smallcloud[i]] = CRGB::DarkGray;
+  }
+  for(int i=0; i<5; i++){
+    icon_leds[drizzle[i]] = CRGB::Cyan;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displayRain(){
+  //Rain
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<25; i++){
+    icon_leds[condition_cloud[i]] = CRGB::RoyalBlue;
+  }
+  for(int i=0; i<9; i++){
+    icon_leds[condition_smallcloud[i]] = CRGB::DarkGray;
+  }
+  for(int i=0; i<17; i++){
+    icon_leds[rain[i]] = CRGB::Cyan;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displayThunder(){
+  //Thunder
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<25; i++){
+    icon_leds[condition_cloud[i]] = CRGB::RoyalBlue;
+  }
+  for(int i=0; i<9; i++){
+    icon_leds[condition_smallcloud[i]] = CRGB::DarkGray;
+  }
+  for(int i=0; i<22; i++){
+    icon_leds[thunder[i]] = CRGB::Yellow;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displayAtmosphere(){
+  //Atmosphere
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<30; i++){
+    icon_leds[atmosphere[i]] = CRGB::Gray;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displaySnow(){
+  //Snow
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Black);
+  for(int i=0; i<19; i++){
+    icon_leds[snow_frame[i]] = CRGB::SlateGray;
+  }
+  for(int i=0; i<36; i++){
+    icon_leds[snow_flake[i]] = CRGB::White;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displayAdditional(){
+  //Additional
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Yellow);
+  for(int i=0; i<38; i++){
+    icon_leds[AE_void[i]] = CRGB::Black;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
+
+void displayExtreme(){
+  //Extreme
+  fill_solid(icon_leds, NUM_LEDS, CRGB::Red);
+  for(int i=0; i<38; i++){
+    icon_leds[AE_void[i]] = CRGB::Black;
+  }
+  FastLED.show();
+  FastLED.delay(10000);
+}
